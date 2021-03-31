98cc80818faed8492fdf413e199e3e3fe68a75a1
shellystarss <56352140+shellystarss@users.noreply.github.com>
2021-03-31 17:39:23 +0700

    Add files via upload

---
+++ suburtest/suburtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  suburtest
+//
+//  Created by Shelina Linardi on 31/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
